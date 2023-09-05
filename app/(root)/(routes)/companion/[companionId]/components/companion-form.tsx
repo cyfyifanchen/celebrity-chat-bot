7a1a82c7b2ed71713fbbe73d1cbc0f7a198355d5
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-05 20:13:38 +0800

    Update companion-form.tsx

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -4,10 +4,12 @@ import * as z from "zod"
 import { Category, Companion } from "@prisma/client"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Separator } from "@/components/ui/separator"
 import { ImageUpload } from "@/components/image-upload"
 import { Input } from "@/components/ui/input"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+
 
 interface CompanionFormProps {
   initialData: Companion | null;
@@ -99,6 +101,95 @@ export const CompanionForm = ({
                       {...field}
                     />
                   </FormControl>
+                  <FormDescription>
+                    This is how your AI Companion will be named.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="description"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="CEO & Founder of Tesla, SpaceX, Neuralink, and The Boring Company"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Description of your AI Companion.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="categoryId"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    disabled={isLoading}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="bg-background">
+                        <SelectValue
+                          defaultValue={field.value}
+                          placeholder="Select a category"
+                        />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {categories.map((category) => (
+                        <SelectItem
+                          key={category.id}
+                          value={category.id}
+                        >
+                          {category.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="CEO & Founder of Tesla, SpaceX, Neuralink, and The Boring Company"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Description of your AI Companion.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="dscription"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="CEO & Founder of Tesla, SpaceX, Neuralink, and The Boring Company"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Description of your AI Companion.
+                  </FormDescription>
+                  <FormMessage />
                 </FormItem>
               )}
             />
