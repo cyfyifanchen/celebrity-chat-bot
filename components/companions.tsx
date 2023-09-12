dfcffb88afd5500d4dc14303a8f64ae114cf7c95
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-12 14:12:06 +0800

    Adding components

---
+++ components/companions.tsx
@@ -0,0 +1,34 @@
+import { Companion } from "@prisma/client"
+import Image from "next/image"
+
+interface CompanionProps {
+  data: (Companion & {
+    _count: {
+      messages: number
+    }
+  })[]
+}
+
+export const Companions = ({
+  data
+}: CompanionProps) => {
+  if (data.length === 0) {
+    return (
+      <div className="pt-10 flex-col items-center justify-center space-y-3">
+        <div className="relative w-60 h-60">
+          <Image
+            fill
+            className="grayscale"
+            alt="Empty"
+            src="/empty.png"
+          />
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div>
+      Companions
+    </div>
+  )
+}
