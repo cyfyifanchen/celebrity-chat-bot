dc8a8bbf8e5d030e421e9337dfec2a045abf646b
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-13 15:42:04 +0800

    Fixing the typo which made the border bottom not showing

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -17,7 +17,7 @@ const font = Poppins({
 
 export const NavBar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 broder-b border-primary/10 bg-secondary h-16">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-collapse-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
                 <MobileSiderbar />
                 <Link href="/">
