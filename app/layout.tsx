92af866e118410394bd910eaf645a8d9be345c4a
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-09-18 18:10:57 +0800

    Adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/react"
+import "./globals.css"
+import type { Metadata } from "next"
 // import { Lateef } from 'next/font/google'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
 
-import { ThemeProvider } from '@/components/theme-provider'
-import { cn } from '@/lib/utils'
-import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/toaster"
 
-const inter = Inter({ subsets: ['latin'], weight: '800' })
+const inter = Inter({ subsets: ["latin"], weight: "800" })
 
 export const metadata: Metadata = {
-  title: 'Celebrity Kombat'
+  title: "Celebrity Kombat",
 }
 
 export default function RootLayout({
@@ -23,12 +24,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn("bg-secondary", inter.className)}>
-          <ThemeProvider attribute="class" defaultTheme='system' enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
+            <Analytics />
             <Toaster />
           </ThemeProvider>
         </body>
       </html>
-    </ClerkProvider >
+    </ClerkProvider>
   )
 }
